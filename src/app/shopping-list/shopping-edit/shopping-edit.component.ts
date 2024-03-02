fc6c254893d1491d73753294d11bda8270fb6b45
beartato327 <recapurro@outlook.com>
2024-03-01 22:36:15 -0600

    finished TD approach, now onto reactive approach

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -39,7 +39,23 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   onAddItem(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
-    this.slService.addIngredient(newIngredient);
+    if(this.editMode){
+      this.slService.updateIngredient(this.itemIndex, newIngredient);
+    }else{
+      this.slService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
+    form.reset();
+  }
+
+  onDelete(){
+    this.slService.deleteIngredient(this.itemIndex);
+    this.onClear();
+  }
+
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
   }
 
   ngOnDestroy(): void {
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -18,6 +18,17 @@ export class ShoppingListService{
       return this.ingredients[index];
     }
 
+    deleteIngredient(index: number){
+      this.ingredients.splice(index, 1);
+      this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index: number, newIngredient: Ingredient){
+      console.log(index)
+      this.ingredients[index] = newIngredient;
+      this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
     addIngredient(ingredient: Ingredient){
       this.ingredients.push(ingredient);
       this.ingredientsChanged.next(this.ingredients.slice());
